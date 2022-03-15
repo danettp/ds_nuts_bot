c14d55c65b9147493dab1e1880a10a89c3169869
danettp <17204@my.sanctamaria.school.nz>
2022-03-16 09:49:23 +1300

    Added comments for each section
    Comments for each section in ds_nuts_bot

---
+++ ds_nuts_bot.py
@@ -0,0 +1,70 @@
+# D's Nuts Bot program
+
+# Welcome message with random name
+
+
+
+
+
+
+# Menu for delivery or click and collect
+
+
+
+
+
+
+# Delivery information - name, phone number, and address
+
+
+
+
+
+
+# Delivery information - name and phone
+
+
+
+
+
+# Choose total number of items - max 5
+
+
+
+
+
+
+# Nuts Menu
+
+
+
+
+
+
+# Item (nuts) order - from menu - print each nut ordered with cast
+
+
+
+
+
+# Print order out - including if the order is delivery or click and collect and names 
+# and price of each nut - total cost including any delivery charge
+
+
+
+
+# Ability to cancel or proceed with order
+
+
+
+
+
+# Option for new order or to exit
+
+
+
+
+
+
+
+# Main function 
