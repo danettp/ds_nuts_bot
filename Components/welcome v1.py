a22987169009abd3ab4c8415a96f3cad62b8ef81
danettp <17204@my.sanctamaria.school.nz>
2022-03-16 09:35:17 +1300

    Created welcome v1
    Created welcome v1 under components folder and created ds_nuts_bot.py

---
+++ Components/welcome v1.py
@@ -0,0 +1,13 @@
+import random
+from random import randint 
+
+#List of random names
+names = ["Jack", "Rachel", "Michelle", "Randy", "Samuel", "An", "Julie", "Jerry", "Jace", "Sofia"]
+
+num = randint(0,9)
+
+name = (names[num])
+
+print("*** Welcome to D's Nuts ***")
+print("*** My name is", name, " ***")
+print("*** I will be here to help you order your delicious organic nuts ***")
