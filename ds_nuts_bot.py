52aa2286b0d201ca80dd3d1f378f2e060cce5a3f
danettp <17204@my.sanctamaria.school.nz>
2022-03-18 14:55:33 +1300

    Integrated click and collect or delivery menu to main app.
    Made a click and collect or delivery menu into a function and integrated into main routine.

--- ds_nuts_bot.py
+++ ds_nuts_bot.py
@@ -22,7 +22,28 @@ def welcome():
 
 
 # Menu for delivery or click and collect
+def collect_menu():
+    print ("Is your order for click and collect or delivery?")
 
+    print("For click and collect please enter 1")
+    print("For delivery please enter 2")
+
+    while True:
+        try: 
+            delivery = int(input("Please enter a number "))
+            if delivery >= 1 and delivery <= 2:
+                if delivery == 1:
+                    print("Click and Collect")
+                    break
+                
+                elif delivery == 2:
+                    print("Delivery")
+                    break
+            else: 
+                print("The number must be 1 or 2")
+        except ValueError:
+            print("That is not a valid number.")
+            print("Please enter 1 or 2")
 
 
 
@@ -89,6 +110,7 @@ def main():
     Returns: None
     '''
     welcome()
+    collect_menu()
 
 
 main()
