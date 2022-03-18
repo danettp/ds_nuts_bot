95c95155f6fb8a65ec2cc96b583d46ac43412131
danettp <17204@my.sanctamaria.school.nz>
2022-03-18 14:29:58 +1300

    Created click & collect or delivery menu version 1
    Basic instructions and print statements for click and collect or delivery menu.

---
+++ Components/candc_delivery_menu v1.py
@@ -0,0 +1,19 @@
+
+# Bugs
+# will only work for valid input "d" and "c"
+
+# menu so that user can choose either click and collect or delivery
+
+print ("Do you want your order delivered or are you click and collecting it?")
+
+print("For delivery press enter d or enter c for click & collect.")
+
+delivery = input()
+
+if delivery == "d":
+    print("Delivery")
+
+elif delivery == "c":
+    print("Click & Collect")
+
+    
