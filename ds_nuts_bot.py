d977a3f2c54e13a2757c1b7988dbaa84a03a3ece
danettp <17204@my.sanctamaria.school.nz>
2022-03-22 20:09:48 +1300

    Integrated confirm_cancel into main app.
    Turned confirm_cancel into a function and integrated into main program.

--- ds_nuts_bot.py
+++ ds_nuts_bot.py
@@ -191,8 +191,31 @@ def print_order(del_collect):
     print()
     print("Total Order Cost")
     print(f"${total_cost:.2f}")
+    print("")
 
 # Ability to cancel or proceed with order
+def confirm_cancel():
+    print ("Please confirm your order - Choose: ")
+    print("(1) To Confirm")
+    print("(2) To Cancel")
+    while True:
+        try: 
+            confirm = int(input("Please enter a number "))
+            if confirm >= 1 and confirm <= 2:
+                if confirm == 1:
+                    print("Order Confirmed")
+                    print("Your order for nuts is getting packed and prepared.")
+                    break
+                
+                elif confirm == 2:
+                    print("Your Order has been Cancelled")
+                    print("You can restart your order or exit the BOT")
+                    break
+            else: 
+                print("The number must be 1 or 2")
+        except ValueError:
+            print("That is not a valid number.")
+            print("Please enter 1 or 2")
 
 
 
@@ -219,6 +242,7 @@ def main():
     menu()
     order_nuts()
     print_order(del_collect)
+    confirm_cancel()
 
 
 main()
